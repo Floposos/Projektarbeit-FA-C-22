2105029d8bd1c21610a5e92287394a72f287f6d8
Floposos <floriheineer@gmail.com>
2024-12-17 11:16:54 +0100

    DBConnection.java class

--- src/DBConnection/DBConnection.java
+++ src/DBConnection/DBConnection.java
@@ -1,6 +1,18 @@
 package DBConnection;
 
+import org.sqlite.JDBC;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class DBConnection {
 
+   String url = "jdbc:sqlite:C://Users//Flo//OneDrive - OSZ IMT//LF12//PROJEKT ARBEIT//DB";
 
+   Connection connection = DriverManager.getConnection(url);
+
+
+    public DBConnection() throws SQLException {
+    }
 }
