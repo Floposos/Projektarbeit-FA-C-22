91f90bd2c505933e3dfd63189c32b940b238dbc0
Floposos <floriheineer@gmail.com>
2024-12-17 12:55:56 +0100

    DBConnection.java class

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,19 @@
+import DBConnection.DBConnection;
+
+import java.sql.SQLException;
+import java.sql.SQLOutput;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+
+        try {
+            DBConnection.Verbidung();
+            System.out.println("Verbunden");
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
 
 
 
